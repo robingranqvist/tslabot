b515f98574405afa5e5ea699982b729ea758b0eb
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2020-08-24 15:31:17 +0300

    second

--- index.js
+++ index.js
@@ -10,26 +10,28 @@ const url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const price;
+let price;
+let percentage;
 
 axios.get('https://stocktwits.com/symbol/TSLA')
     .then(res => {
         const $ = cheerio.load(res.data);
 
         price = $('.st_3zYaKAL').text();
-        const percentage = $('.st_3Z2BeqA').text();
+        percentage = $('.st_3Z2BeqA').text();
         console.log("Pris:", price);
         console.log(percentage);
-    });
-
-let TSLA;
-
-const data = fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        TSLA = Math.floor(data['Time Series (Daily)']['2020-08-21']['4. close']);
     })
     .catch(err => console.log(err));
+
+// let TSLA;
+
+// const data = fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         TSLA = Math.floor(data['Time Series (Daily)']['2020-08-21']['4. close']);
+//     })
+//     .catch(err => console.log(err));
 
 client.once('ready', () => {
     console.log("Ready");
@@ -42,21 +44,21 @@ client.on('message', message => {
         message.channel.send(price);
     }
 
-    if(message.content.startsWith(`${prefix}fredda`)) {
-        message.channel.send("FREDDA ÄGER: " + "$" + TSLA * 16);
-    }
+    // if(message.content.startsWith(`${prefix}fredda`)) {
+    //     message.channel.send("FREDDA ÄGER: " + "$" + TSLA * 16);
+    // }
 
-    if(message.content.startsWith(`${prefix}vinst`)) {
-        message.channel.send("FREDDAS VINST: " + "$" + (TSLA * 16 - 5000) );
-    }
+    // if(message.content.startsWith(`${prefix}vinst`)) {
+    //     message.channel.send("FREDDAS VINST: " + "$" + (TSLA * 16 - 5000) );
+    // }
 
-    if(message.content.startsWith(`${prefix}pedo`)) {
-        message.channel.send("https://ibb.co/DwBymd0");
-    }
+    // if(message.content.startsWith(`${prefix}pedo`)) {
+    //     message.channel.send("https://ibb.co/DwBymd0");
+    // }
 
-    if(message.content.startsWith(`${prefix}help`)) {
-        message.channel.send("Men pappa ja vilit.");
-    }
+    // if(message.content.startsWith(`${prefix}help`)) {
+    //     message.channel.send("Men pappa ja vilit.");
+    // }
 });
 
 client.login(token);
