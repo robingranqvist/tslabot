8cfb9fc164a9f31138abcf10c54a37543f9b53be
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2020-08-24 22:00:55 +0300

    second

--- index.js
+++ index.js
@@ -24,11 +24,22 @@ const url = 'https://www.stockmonitor.com/quote/tsla/';
 app.listen(PORT, () => {
 
     client.on('ready', () => {
-        client.user.setPresence({ activity: { name: 'monies 🤑' }, status: 'Making' })
+        client.user.setPresence({ activity: { name: 'monies 🤑', type: 'Making' }, status: 'online' })
         .then(console.log)
         .catch(console.error);
         
         setTimeout(function(){ console.log("Hello"); }, 900000);
+    });
+    // Discord
+    client.once('ready', () => {
+        client.user.setPresence({
+            game: {
+              name: 'Monies 🤑',
+              type: 'Making',
+            },
+            status: 'online',
+        });
+
     });
 
     client.on('message', message => {
