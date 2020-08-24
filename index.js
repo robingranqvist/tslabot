0a249f0eab7158af7759cf2756328e6dcd4913be
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2020-08-24 15:34:17 +0300

    second

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
-// API
-const fetch = require('node-fetch');
-const url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=349CBXO0QJ1EIWJX";
-
 // Scrape
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -38,27 +34,10 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message.content);
 
     if(message.content.startsWith(`${prefix}tsla`)) {
         message.channel.send(price);
     }
-
-    // if(message.content.startsWith(`${prefix}fredda`)) {
-    //     message.channel.send("FREDDA ÄGER: " + "$" + TSLA * 16);
-    // }
-
-    // if(message.content.startsWith(`${prefix}vinst`)) {
-    //     message.channel.send("FREDDAS VINST: " + "$" + (TSLA * 16 - 5000) );
-    // }
-
-    // if(message.content.startsWith(`${prefix}pedo`)) {
-    //     message.channel.send("https://ibb.co/DwBymd0");
-    // }
-
-    // if(message.content.startsWith(`${prefix}help`)) {
-    //     message.channel.send("Men pappa ja vilit.");
-    // }
 });
 
 client.login(token);
