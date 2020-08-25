cb1d725d7f58d41a892be6f4ee3b8d14e421da51
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2020-08-25 20:15:50 +0300

    Yo

--- index.js
+++ index.js
@@ -45,17 +45,17 @@ app.listen(PORT, () => {
                 let currentNegative = oldPriceLast - c;
                 let currentPositive = c - oldPriceLast;
                 if (oldPriceArr.length > 2) {
-                    if ((oldPriceLast - c) > 1) {
+                    if ((oldPriceLast - c) > 5) {
                         channel.send("ALERT, WE'RE GOING DOWN BOIS! " + "-$" + currentNegative);
-                    } else if ((c - oldPriceLast) > 1) {
+                    } else if ((c - oldPriceLast) > 5) {
                         channel.send("WE'RE GOING UP BOIS! " + "+$" + currentPositive);
-                    } else {
-                        channel.send("Current price is " + "$" + c);
-                    }
+                    } //else {
+                    //     channel.send("Current price is " + "$" + c);
+                    // }
                 }
                 console.log(c, oldPriceLast);
             });
-        }, 180000);
+        }, 60000);
     });
     
     async function getTsla() {
